f219db10c3d74351868b22e09c8e20736692b9f6
Augusto Lopez <auguston8@hotmail.com>
2024-07-28 23:07:38 -0300

    Atualizando projeto ...

--- app.js
+++ app.js
@@ -73,6 +73,6 @@ function reiniciarJogo() {
   tentativas = 1;
   //exibirMensagemInicial();
   exibirTextoNaTela("h1", "Jogo do número secreto");
-  exibirTextoNaTela("p", "Escolha um número entre 1 e 100");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
   document.getElementById("reiniciar").setAttribute("disabled", true);
 }
